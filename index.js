fb56c0363efe5869dc4961ec3d77a2ea7e39b6fb
Brian Garland <briangarlandk@gmail.com>
2021-12-11 22:45:39 -0600

    Added prompts to index

---
+++ index.js
@@ -0,0 +1,143 @@
+const inquirer = require('inquirer');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+const results = []
+
+const run = () => {
+    console.log('Welcome to the team generator! \n\nUse npm reset to reset the dist/ folder \n')
+    runInquirer()
+}
+
+const managerPrompts = () => {
+    inquirer
+    .prompt([{
+        type: 'input',
+        message: "What is your team manager's name",
+        name: 'manager_name'
+    },
+    {
+        type: 'input',
+        message: "What is your team manager's id?",
+        name: 'manager_id'
+    },
+    {
+        type: 'input',
+        message: "What is your team manager's email?",
+        name: 'manager_email'
+    },
+    {
+        type: 'input',
+        message: "What is your team manager's office number?",
+        name: 'manager_office_number'
+    }
+    ]).then(({manager_id, manager_name, manager_email, manager_office_number}) =>{
+        const manager = new Manager(manager_id, manager_name, manager_email, manager_office_number)
+
+        if(manager){
+            results.push({...manager, type: 'manager'}) 
+            getNextMember()
+        }
+    })
+}
+
+const engineerPrompts = () => {
+    inquirer
+    .prompt([{
+        type: 'input',
+        message: "What is your team engineer's name",
+        name: 'engineer_name'
+    },
+    {
+        type: 'input',
+        message: "What is your team engineer's id?",
+        name: 'engineer_id'
+    },
+    {
+        type: 'input',
+        message: "What is your team engineer's email?",
+        name: 'engineer_email'
+    },
+    {
+        type: 'input',
+        message: "What is your team engineer's Github username?",
+        name: 'engineer_github'
+    }
+    ]).then(({engineer_id, engineer_name, engineer_email, engineer_github}) =>{
+        const engineer = new Engineer(engineer_id, engineer_name, engineer_email, engineer_github)
+
+        if(engineer){
+            results.push({...engineer, type: 'engineer'}) 
+            getNextMember()
+        }
+    })
+}
+
+const internPrompts = () => {
+    inquirer
+    .prompt([{
+        type: 'input',
+        message: "What is your team intern's name",
+        name: 'intern_name'
+    },
+    {
+        type: 'input',
+        message: "What is your team intern's id?",
+        name: 'intern_id'
+    },
+    {
+        type: 'input',
+        message: "What is your team intern's email?",
+        name: 'intern_email'
+    },
+    {
+        type: 'input',
+        message: "What is your team intern's school name?",
+        name: 'intern_school'
+    }
+    ]).then(({intern_id, intern_name, intern_email, intern_school}) =>{
+        const intern = new Intern(intern_id, intern_name, intern_email, intern_school)
+
+        if(intern){
+            results.push({...intern, type: 'intern'}) 
+            getNextMember()
+        }
+    })
+}
+
+const getNextMember = () => {
+    inquirer
+    .prompt([{
+        type: 'list',
+        message: "Which team member would you like to add next?",
+        name: 'member_pick',
+        choices: [
+            'Engineer',
+            'Intern',
+            'I dont want to add any more team members'
+        ]
+    }
+    
+    ]).then(({member_pick}) =>{
+
+
+        switch(member_pick){
+            case 'Engineer':
+                return engineerPrompts()
+            case 'Intern':
+                return internPrompts()
+            case 'I dont want to add any more team members':
+                // use data from results object for fs 
+                return console.log(results)
+        }
+    })
+}
+
+const runInquirer = () => {
+    managerPrompts()
+    
+}
+
+
+run()
